3f0f94763e4e5795330e5b4f8c5b41b1e205a264
redth <jondick@gmail.com>
2026-06-11 21:50:21 -0400

    chore: drop one-off cert-dump debug scripts

--- tools/interop-controller/dumpnoc.mjs
+++
@@ -1,5 +0,0 @@
-import { Noc, DerCodec } from "@matter/protocol";
-const hex = process.argv[2];
-const noc = Noc.fromTlv(Buffer.from(hex, "hex"));
-const tbs = DerCodec.encode(noc.genericBuildAsn1Structure(noc.cert));
-console.log(Buffer.from(tbs).toString("hex"));
